0a0a588fea1b5ef7dce2aaab15e11dd86aeaacc0
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-25 17:42:16 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -18,9 +18,10 @@ require 'pry'
     movie = Movie.create(attributes)
   end
 
-  def can_be_created_in_a_block(args)
+  def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
     Movie.create do |m|
-      m.title = args[0]
+      m.title = args[:title]
+      m.release_date = args[:release_date]
 
       #binding.pry
     end
